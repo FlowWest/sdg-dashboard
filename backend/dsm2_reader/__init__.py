0973b6474235a76ef80c038e05019bdb3607e4e3
ergz <erodriguez@flowwest.com>
2024-12-11 15:36:21 -0800

    adds reading the echo file weir gate config section

--- backend/dsm2_reader/__init__.py
+++ backend/dsm2_reader/__init__.py
@@ -2,7 +2,38 @@ from hecdss import HecDss
 import pandas as pd
 
 
-def get_all_data(dss):
+def read_echo_file(filepath: str):
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    start_index = next(
+        (i for i, line in enumerate(lines) if "GATE_WEIR_DEVICE" in line), None
+    )
+    end_index = next(
+        (i for i, line in enumerate(lines) if "END" in line and i > start_index), None
+    )
+    print(f"the start index = {start_index}\nthe end index = {end_index}")
+
+    if start_index is not None and end_index is not None:
+        data_rows = lines[start_index + 1 : end_index]
+        col_names = [
+            "GATE_NAME",
+            "DEVICE",
+            "NDUPLICATE",
+            "WIDTH",
+            "ELEV",
+            "HEIGHT",
+            "CF_FROM_NODE",
+            "CF_TO_NODE",
+            "DEFAULT_OP",
+        ]
+
+        data = [line.split(maxsplit=len(col_names) - 1) for line in data_rows[1:]]
+        df = pd.DataFrame(data, columns=col_names)
+        return df
+
+
+def get_all_data_from_dsm2_dss(dss):
     out = {}
     cat = dss.get_catalog()
     paths = list(cat.recordTypeDict.keys())
