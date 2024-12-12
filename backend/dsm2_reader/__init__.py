4c5a1e5e1e8e20cd96f9cb2ab1b789e678b1ae0a
ergz <erodriguez@flowwest.com>
2024-12-11 22:18:47 -0800

    adds a way to add different parts from the dss

--- backend/dsm2_reader/__init__.py
+++ backend/dsm2_reader/__init__.py
@@ -12,7 +12,6 @@ def read_echo_file(filepath: str):
     end_index = next(
         (i for i, line in enumerate(lines) if "END" in line and i > start_index), None
     )
-    print(f"the start index = {start_index}\nthe end index = {end_index}")
 
     if start_index is not None and end_index is not None:
         data_rows = lines[start_index + 1 : end_index]
@@ -33,14 +32,19 @@ def read_echo_file(filepath: str):
         return df
 
 
-def get_all_data_from_dsm2_dss(dss):
+def get_all_data_from_dsm2_dss(
+    dss, part_b="gate", part_c="parameter", part_f="scenario"
+):
     out = {}
     cat = dss.get_catalog()
-    paths = list(cat.recordTypeDict.keys())
-    for path in paths:
+    paths = list(cat.recordtypedict.keys())
+    for i, path in enumerate(paths):
         path_data = dss.get(path)
-        out[path] = pd.DataFrame(
+        out[path] = pd.dataframe(
             {
+                part_b: cat.items[i].b,
+                part_c: cat.items[i].c,
+                part_f: cat.items[i].f,
                 "datetime": path_data.get_dates(),
                 "value": path_data.get_values(),
                 "unit": path_data.units,
