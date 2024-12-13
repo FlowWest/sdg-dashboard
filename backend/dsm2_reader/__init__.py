c9c15abd05585cdeab71cfc992da0578b5360909
ergz <erodriguez@flowwest.com>
2024-12-13 15:39:20 -0800

    start to organize all functions

--- backend/dsm2_reader/__init__.py
+++ backend/dsm2_reader/__init__.py
@@ -1,7 +1,103 @@
 from hecdss import HecDss
 import pandas as pd
+from dataclasses import dataclass
+import os
 
 from typing import Dict
+
+
+@dataclass
+class SDGScenario:
+    path: str
+    name: str
+
+    def __locate_files(self):
+        output_files = os.listdir("output")
+        echo_file = [
+            f"output/{f}" for f in output_files if "echo" in f and self.name in f
+        ]
+        dss_files = [
+            f"output/{f}" for f in output_files if "dss" in f and self.name in f
+        ]
+
+        return {"echo": echo_file, "dss": dss_files}
+
+    def __read_echo_file(self, filepath: str):
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+
+        start_index = next(
+            (i for i, line in enumerate(lines) if "GATE_WEIR_DEVICE" in line), None
+        )
+        end_index = next(
+            (i for i, line in enumerate(lines) if "END" in line and i > start_index),
+            None,
+        )
+
+        if start_index is not None and end_index is not None:
+            data_rows = lines[start_index + 1 : end_index]
+            col_names = [
+                "GATE_NAME",
+                "DEVICE",
+                "NDUPLICATE",
+                "WIDTH",
+                "ELEV",
+                "HEIGHT",
+                "CF_FROM_NODE",
+                "CF_TO_NODE",
+                "DEFAULT_OP",
+            ]
+
+            data = [line.split(maxsplit=len(col_names) - 1) for line in data_rows[1:]]
+            df = pd.DataFrame(data, columns=col_names)
+            df = df[df["DEVICE"] == "fish_passage"]
+            return df
+
+    def __post_init__(self):
+        self.files = self.__locate_files()
+        self.echo = self.__read_echo_file(self.files.get("echo")[0])
+
+    def read_dss(
+        self,
+        dss: HecDss,
+        part_names: Dict[str, str] | None = None,
+        concat: bool = False,
+    ) -> pd.DataFrame | Dict[str, pd.DataFrame]:
+        out = {}
+        cat = dss.get_catalog()
+        paths = list(cat.recordTypeDict.keys())
+        for i, path in enumerate(paths):
+            path_data = dss.get(path)
+            out[path] = pd.DataFrame(
+                {
+                    "datetime": path_data.get_dates(),  # type: ignore
+                    "value": path_data.get_values(),  # type: ignore
+                    "unit": path_data.units,  # type: ignore
+                }
+            )
+            if part_names is not None:
+                if "A" in part_names:
+                    out[path][part_names.get("A")] = cat.items[i].A
+                if "B" in part_names:
+                    out[path][part_names.get("B")] = cat.items[i].B
+                if "C" in part_names:
+                    out[path][part_names.get("C")] = cat.items[i].C
+                if "D" in part_names:
+                    out[path][part_names.get("D")] = cat.items[i].D
+                if "E" in part_names:
+                    out[path][part_names.get("E")] = cat.items[i].E
+                if "F" in part_names:
+                    out[path][part_names.get("F")] = cat.items[i].F
+
+        if concat:
+            out = pd.concat(out.values())
+        return out
+
+
+def read_scenario(path):
+    hydo_dss = ""
+    sdg_dss = ""
+    echo_file = ""
 
 
 def read_echo_file(filepath: str):
@@ -31,6 +127,7 @@ def read_echo_file(filepath: str):
 
         data = [line.split(maxsplit=len(col_names) - 1) for line in data_rows[1:]]
         df = pd.DataFrame(data, columns=col_names)
+        df = df[df["DEVICE"] == "fish_passage"]
         return df
 
 
