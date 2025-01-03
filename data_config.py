ed8295a26e0a71c7d79536893d3675f44d877fce
InigoP <ipeng@flowwest.com>
2025-01-02 16:25:50 -0800

    updated function

--- data_config.py
+++ data_config.py
@@ -8,6 +8,8 @@ gatef = {'name' : ['GrantLine','MiddleRiver','OldRiver'],
          "gate_status" : ["GLC_GATE_UP", "MID_GATE_UP", "OLD_GATE_UP"]
          }
 
+stn_list = ['MID_GATEOP','GLC_GATEOP','OLD_GATEOP']
 elev_list =['MID_GATE_UP','MID_GATE_DOWN','GLC_GATE_UP','GLC_GATE_DOWN','OLD_GATE_UP','OLD_GATE_DOWN']
 flow_list = ['GLC_FLOW_FISH','MID_FLOW_FISH','MID_FLOW_GATE','OLD_FLOW_FISH','OLD_FLOW_GATE']
 stn_name = ['MHO','DGL','OLD']
+
