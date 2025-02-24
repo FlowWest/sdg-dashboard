af35b7e2a22b6ec6afcaef03276d7d255bf91a41
InigoP <ipeng@flowwest.com>
2025-02-24 14:11:02 -0800

    removed file

--- app/secrets.py
+++
@@ -1,7 +0,0 @@
-db_secrets = {
-    "host": "sdg-db.ctfqkslimkx4.us-west-2.rds.amazonaws.com",
-    "database": "postgres",
-    "user": "postgres",
-    "password": "superpassword",
-    "port": 5432
-}
