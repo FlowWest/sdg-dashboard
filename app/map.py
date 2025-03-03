e922cc1d0c085eeddbde96242c7670a1042ad5c1
ergz <erodriguez@flowwest.com>
2025-03-03 14:08:24 -0800

    get map to not re-render every page pan

--- app/map.py
+++ app/map.py
@@ -1,6 +1,6 @@
 import folium
 import streamlit as st
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static, st_folium
 import geopandas as gpd
 
 
@@ -24,12 +24,28 @@ st.title("Map")
 
 col_left, col_right = st.columns([1, 4])
 
-m = folium.Map(
-    location=[
-        dsm2_channels.union_all().centroid.y,
-        dsm2_channels.union_all().centroid.x,
-    ]
-)
+
+def get_or_create_map():
+    if "map" not in st.session_state or st.session_state is None:
+        m = folium.Map(
+            location=[
+                dsm2_channels.union_all().centroid.y,
+                dsm2_channels.union_all().centroid.x,
+            ],
+            width="100%",
+            height="100%",
+        )
+        channel_lines = dsm2_channels[dsm2_channels.geometry.type == "LineString"]
+        for index, row in channel_lines.iterrows():
+            locs = [(y, x) for x, y in row.geometry.coords]
+            folium.PolyLine(
+                locations=locs,
+                color="blue",
+                weight=5,
+                tooltip=f"Channel ID: {row['id']}",
+            ).add_to(m)
+        st.session_state.map = m
+    return st.session_state.map
 
 
 with col_left:
@@ -47,10 +63,4 @@ with col_left:
     st.select_slider("Select Year", options=range(2016, 2024))
 
 with col_right:
-    channel_lines = dsm2_channels[dsm2_channels.geometry.type == "LineString"]
-    for index, row in channel_lines.iterrows():
-        locs = [(y, x) for x, y in row.geometry.coords]
-        folium.PolyLine(
-            locations=locs, color="blue", weight=2.5, tooltip=f"Channel ID: {row['id']}"
-        ).add_to(m)
-    st_data = st_folium(m, use_container_width=True)
+    folium_static(get_or_create_map())
