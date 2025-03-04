8b944bf3c68399400b017a480a1782b7624baaf9
ergz <erodriguez@flowwest.com>
2025-03-04 10:31:43 -0800

    wip

--- app/map.py
+++ app/map.py
@@ -2,13 +2,13 @@ import folium
 import streamlit as st
 from streamlit_folium import folium_static, st_folium
 import geopandas as gpd
+import pandas as pd
 
 
 dsm2_channels = gpd.read_file(
     "data-raw/fc2024.01_chan/FC2024.01_channels_centerlines.shp"
 )
 dsm2_channels = dsm2_channels.to_crs(epsg=4326)
-
 month_names = {
     5: "May",
     6: "Jun",
@@ -20,6 +20,17 @@ month_names = {
 }
 
 
+baseline_data = pd.read_csv("baseline-data.csv", parse_dates=["Date"])
+scenario_data = pd.read_csv("fpv2mb-data.csv", parse_dates=["Date"])
+
+baseline_data["year"] = baseline_data["Date"].dt.year
+
+dsm2_channels_with_stage = dsm2_channels
+# .merge(
+#     baseline_data, how="left", left_on="id", right_on="channel_id"
+# )
+
+
 st.title("Map")
 
 col_left, col_right = st.columns([1, 4])
@@ -29,20 +40,21 @@ def get_or_create_map():
     if "map" not in st.session_state or st.session_state is None:
         m = folium.Map(
             location=[
-                dsm2_channels.union_all().centroid.y,
-                dsm2_channels.union_all().centroid.x,
+                dsm2_channels_with_stage.union_all().centroid.y,
+                dsm2_channels_with_stage.union_all().centroid.x,
             ],
             width="100%",
             height="100%",
         )
-        channel_lines = dsm2_channels[dsm2_channels.geometry.type == "LineString"]
+        channel_lines = dsm2_channels_with_stage[
+            dsm2_channels_with_stage.geometry.type == "LineString"
+        ]
         for index, row in channel_lines.iterrows():
             locs = [(y, x) for x, y in row.geometry.coords]
             folium.PolyLine(
                 locations=locs,
                 color="blue",
                 weight=5,
-                tooltip=f"Channel ID: {row['id']}",
             ).add_to(m)
         st.session_state.map = m
     return st.session_state.map
@@ -58,9 +70,10 @@ with col_left:
         Monthly summarize represent the min, max, or mean of the daily summaries.
         """
     )
-    st.selectbox("Select Year", options=range(2016, 2024))
+    selected_year = st.selectbox("Select Year", options=range(2016, 2024))
     st.selectbox("Select Month", options=month_names)
     st.select_slider("Select Year", options=range(2016, 2024))
+    filtered_data = baseline_data[baseline_data["year"] == selected_year]
 
 with col_right:
-    folium_static(get_or_create_map())
+    folium_static(get_or_create_map(), width=1400, height=600)
